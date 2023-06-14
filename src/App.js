e8981c4cd7f07ac6c423d30612cb0656f1c2f5df
Rodrigo Toledo <rodrigo.toledo@gliconline.com.br>
2023-06-14 19:25:12 -0300

    node modules in docker compose app sending multiple files

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 import ProductList from './ProductList';
 
 const App = () => {
-  const [file, setFile] = useState(null);
   const [imports, setImports] = useState([]);
+  const [selectedFiles, setSelectedFiles] = useState([]);
   const serverURL = 'http://localhost:3001';
 
   useEffect(() => {
@@ -21,35 +21,45 @@ const App = () => {
   }, []);
 
   const handleFileChange = (event) => {
-    const selectedFile = event.target.files[0];
-    setFile(selectedFile);
+    const files = Array.from(event.target.files);
+    setSelectedFiles(files);
   };
 
   const handleFileUpload = async () => {
-    if (file) {
-      const formData = new FormData();
-      formData.append('file', file);
+    const uploadedImports = [];
 
-      try {
-        await fetch(`${serverURL}/import`, {
-          method: 'POST',
-          body: formData,
-        });
+    for (const file of selectedFiles) {
+      if (file) {
+        const formData = new FormData();
+        formData.append('file', file);
 
-        setFile(null);
-        window.location.reload();
-      } catch (error) {
-        console.error(error);
+        try {
+          await fetch(`${serverURL}/import`, {
+            method: 'POST',
+            body: formData,
+          });
+
+          uploadedImports.push(file);
+
+          if (uploadedImports.length === selectedFiles.length) {
+            window.location.reload();
+          }
+        } catch (error) {
+          console.error(error);
+        }
       }
     }
+
+    setSelectedFiles([]);
   };
+
 
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="py-8">
         <div className="mb-4">
-          <input type="file" onChange={handleFileChange} />
+          <input type="file" multiple={true} onChange={handleFileChange} />
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"
             onClick={handleFileUpload}
