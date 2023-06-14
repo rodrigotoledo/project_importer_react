d52cd5c246d51f219b087e49827e6c4942eb1d71
Rodrigo Toledo <rodrigo.toledo@gliconline.com.br>
2023-06-14 12:55:01 -0300

    project finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ProductList from './ProductList';
 
-function App() {
+const App = () => {
+  const [file, setFile] = useState(null);
+  const [imports, setImports] = useState([]);
+
+  useEffect(() => {
+    const fetchImports = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/imports');
+        setImports(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchImports();
+  }, []);
+
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+  };
+
+  const handleFileUpload = async () => {
+    if (file) {
+      const formData = new FormData();
+      formData.append('file', file);
+
+      try {
+        await fetch(`${serverURL}/import`, {
+          method: 'POST',
+          body: formData,
+        });
+
+        setFile(null);
+        window.location.reload();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="py-8">
+        <div className="mb-4">
+          <input type="file" onChange={handleFileChange} />
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"
+            onClick={handleFileUpload}
+          >
+            Enviar Arquivo
+          </button>
+        </div>
+
+        <ProductList json={imports} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/ProductList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const ProductList = ({ json }) => {
+  const { product_list, category_weights, weighing_start_date } = json;
+
+  return (
+    <div className="container mx-auto">
+      <h2 className="text-2xl font-bold mb-4">Category Weights</h2>
+      <ul className="list-disc ml-6">
+        {Object.entries(category_weights ?? {}).map(([category, weight]) => (
+          <li key={category}>{category}: {weight} kg</li>
+        ))}
+      </ul>
+
+      <h2 className="text-2xl font-bold my-4">Weighing Start Date</h2>
+      <p>{weighing_start_date}</p>
+
+      <h2 className="text-2xl font-bold my-4">Product List</h2>
+      {Object.entries(product_list ?? {}).map(([category, products]) => (
+        <div key={category}>
+          <h3 className="text-lg font-bold">{category}</h3>
+          <div className="grid grid-cols-1 gap-4">
+            {products.map((product) => (
+              <div key={product.id} className="border border-gray-300 p-4 rounded">
+                <p><span className="font-semibold">Date:</span> {product.date}</p>
+                <p><span className="font-semibold">Category:</span> {product.category}</p>
+                <p><span className="font-semibold">Product ID:</span> {product.product_id}</p>
+                <p><span className="font-semibold">Weight:</span> {product.weight}</p>
+                <p><span className="font-semibold">Unit:</span> {product.unit}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
